e47dde9c025ba705a473f3c5bca15be41efe2e19
Deepak Kumar <deepakplay14@gmail.com>
2024-09-06 17:01:59 +0530

    String permutation using swaping

--- recursion/BackTracking.java
+++ recursion/BackTracking.java
@@ -4,24 +4,46 @@ public class BackTracking {
 
     public static void main(String[] args) {
         permutation("ABC".toCharArray(), 0);
+        System.err.println();
+        permutationSwap("ABC".toCharArray(), 0);
+
     }
 
     private static int[] state = new int[10];
     private static char[] statestr = new char[10];
 
-    public static void permutation(char[] str, int num) {
-        if (num == str.length) {
+    public static void permutation(char[] str, int level) {
+        if (level == str.length) {
             System.out.println(new String(statestr));
             return;
         }
 
         for (int i = 0; i < str.length; i++) {
             if (state[i] == 0) {
-                statestr[num] = str[i];
+                statestr[level] = str[i];
                 state[i] = 1;
-                permutation(str, num + 1);
+                permutation(str, level + 1);
                 state[i] = 0;
             }
         }
+    }
+
+    public static void permutationSwap(char[] str, int level) {
+        if (level == str.length) {
+            System.out.println(new String(str));
+            return;
+        }
+
+        for (int i = level; i < str.length; i++) {
+            swap(str, level, i);
+            permutationSwap(str, level + 1);
+            swap(str, level, i);
+        }
+    }
+
+    private static void swap(char[] str, int i1, int i2) {
+        char temp = str[i1];
+        str[i1] = str[i2];
+        str[i2] = temp;
     }
 }
