60091335db53d41fdb57d1b33bfa10e69ce70fa9
Deepak Kumar <deepakplay14@gmail.com>
2024-09-14 11:19:02 +0530

    Top to bottom pattern using recursion

--- recursion/PrintTriangle.java
+++ recursion/PrintTriangle.java
@@ -3,6 +3,7 @@ package recursion;
 public class PrintTriangle {
     public static void main(String[] args) {
         triangle(10, 0);
+        triangleTop(0, 0, 10);
     }
 
     public static void triangle(int row, int col) {
@@ -16,6 +17,20 @@ public class PrintTriangle {
         } else {
             System.out.println();
             triangle(row - 1, 0);
+        }
+    }
+
+    public static void triangleTop(int row, int col, int n) {
+        if (row == n) {
+            return;
+        }
+
+        if (col <= row) {
+            System.out.print(" * ");
+            triangleTop(row, col + 1, n);
+        } else {
+            System.out.println();
+            triangleTop(row + 1, 0, n);
         }
     }
 }
