3a741d93ecf4d2e64b837c35ed12850dee74d1d7
Deepak Kumar <deepakplay14@gmail.com>
2024-09-18 19:57:02 +0530

    V pattern

--- pattern/Simple.java
+++ pattern/Simple.java
@@ -344,5 +344,21 @@ public class Simple {
 
             System.out.println();
         }
+
+        System.out.println();
+        /////////////////////////////////////
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int j = 1; j <= (num - i) * 2; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j > 0; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
 }
