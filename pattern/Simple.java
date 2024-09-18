b98751f4d5175ecd958bd5c61a9c5a5d82c9bd5a
Deepak Kumar <deepakplay14@gmail.com>
2024-09-18 19:52:07 +0530

    Number triangle

--- pattern/Simple.java
+++ pattern/Simple.java
@@ -325,5 +325,24 @@ public class Simple {
 
             System.out.println();
         }
+
+        System.out.println();
+        /////////////////////////////////////
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = i; j > 0; j--) {
+                System.out.print(j);
+            }
+
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
     }
 }
