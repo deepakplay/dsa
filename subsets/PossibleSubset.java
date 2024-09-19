6791c4f84612add5c611b0d9c2cfd5c42ad86615
Deepak Kumar <deepakplay14@gmail.com>
2024-09-20 00:07:57 +0530

    Subset

---
+++ subsets/PossibleSubset.java
@@ -0,0 +1,53 @@
+package subsets;
+
+import java.util.*;
+
+class Possible {
+
+    public static void main(String[] args) {
+
+        System.out.println(substr("", "aabc"));
+        System.out.println(substrItr("aabc"));
+    }
+
+    public static ArrayList<String> substr(String str, String src) {
+        if (src.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(str);
+            return list;
+        }
+
+        char ch = src.charAt(0);
+        ArrayList<String> list1 = substr(ch + str, src.substring(1));
+        ArrayList<String> list2 = substr(str, src.substring(1));
+        list1.addAll(list2);
+        return list1;
+    }
+
+    public static List<List<Character>> substrItr(String str) {
+        List<List<Character>> list = new ArrayList<>();
+
+        list.add(new ArrayList<>());
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (i > 0 && str.charAt(i) == str.charAt(i - 1)) {
+                start = end + 1;
+            }else{
+                start = 0;
+            }
+            end = list.size() - 1;
+
+            for (int j = start; j <= end; j++) {
+                List<Character> newList = new ArrayList<>(list.get(j));
+                newList.add(str.charAt(i));
+                list.add(newList);
+            }
+        }
+
+        return list;
+    }
+}
